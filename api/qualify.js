cff8868f61475d422b75f6207cecf8bc55e9af17
theroadprovides <theroadprovidesofficial@gmail.com>
2026-09-18 22:55:01 -0300

    Update qualify.js

--- api/qualify.js
+++ api/qualify.js
@@ -12,7 +12,7 @@ const TOTAL_SPOTS = 100;
 // Exemplo:
 // 3_000_000 = 3 milhões de $ROAD
 //
-const MIN_ROAD_REQUIRED = 3_000_000;
+const MIN_ROAD_REQUIRED = 30;
 
 const TOKEN_MINT =
   "BgVkpGKLuiUGwj4GzaYyoKbWNMBUeem8rpuvEuRApump";
